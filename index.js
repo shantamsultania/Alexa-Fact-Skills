07ab4ada5e1d439b8b5cbabd8cee80dacdf3d3d9
shantamsultania <shantam1230@gmail.com>
2020-04-16 04:49:16 +0530

    sample fact skill for alexa

---
+++ index.js
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+//
+// Alexa Fact Skill - Sample for Beginners
+//
+
+// sets up dependencies
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+
+// core functionality for fact skill
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    // checks request type
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    // gets a random fact by assigning an array to the variable
+    // the random item from the array will be selected by the i18next library
+    // the i18next library is set up in the Request Interceptor
+    const randomFact = requestAttributes.t('FACTS');
+    // concatenates a standard message with the random fact
+    const speakOutput = requestAttributes.t('GET_FACT_MESSAGE') + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      // Uncomment the next line if you want to keep the session open so you can
+      // ask for another fact without first re-opening the skill
+      // .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .withSimpleCard(requestAttributes.t('SKILL_NAME'), randomFact)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const FallbackHandler = {
+  // The FallbackIntent can only be sent in those locales which support it,
+  // so this handler will always be skipped in locales where it is not supported.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    // Gets the locale from the request and initializes i18next.
+    const localizationClient = i18n.init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+      returnObjects: true
+    });
+    // Creates a localize function to support arguments.
+    localizationClient.localize = function localize() {
+      // gets arguments through and passes them to
+      // i18next using sprintf to replace string placeholders
+      // with arguments.
+      const args = arguments;
+      const value = i18n.t(...args);
+      // If an array is used then a random value is selected
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    // this gets the request attributes and save the localize function inside
+    // it to be used in a handler by calling requestAttributes.t(STRING_ID, [args...])
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    }
+  }
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler,
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .withCustomUserAgent('sample/basic-fact/v2')
+  .lambda();
+
+// TODO: Replace this data with your own.
+// It is organized by language/locale.  You can safely ignore the locales you aren't using.
+// Update the name and messages to align with the theme of your skill
+
+const deData = {
+  translation: {
+    SKILL_NAME: 'Weltraumwissen',
+    GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
+    HELP_MESSAGE: 'Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?',
+    HELP_REPROMPT: 'Wie kann ich dir helfen?',
+    FALLBACK_MESSAGE: 'Die Weltraumfakten Skill kann dir dabei nicht helfen. Sie kann dir Fakten über den Raum erzählen, wenn du dannach fragst.',
+    FALLBACK_REPROMPT: 'Wie kann ich dir helfen?',
+    ERROR_MESSAGE: 'Es ist ein Fehler aufgetreten.',
+    STOP_MESSAGE: 'Auf Wiedersehen!',
+    FACTS:
+      [
+        'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
+        'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
+        'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
+        'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
+        'Jupiter hat den kürzesten Tag aller Planeten.',
+      ],
+  },
+};
+
+const dedeData = {
+  translation: {
+    SKILL_NAME: 'Weltraumwissen auf Deutsch',
+  },
+};
+
+const enData = {
+  translation: {
+    SKILL_NAME: 'Enter your skill name',
+    GET_FACT_MESSAGE: 'Here\'s your fact: ',
+    HELP_MESSAGE: 'You can say tell me a computer vision fact, or, you can say exit... What can I help you with?',
+    HELP_REPROMPT: 'What can I help you with?',
+    FALLBACK_MESSAGE: 'The computer vision fact guide skill can\'t help you with that.  It can help you discover facts about computer vision if you say tell me a computer vision fact guide. What can I help you with?',
+    FALLBACK_REPROMPT: 'What can I help you with?',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    STOP_MESSAGE: 'Goodbye!',
+    FACTS:
+      [
+        'enter your data', 
+      ],
+  },
+};
+
+
+
+// constructs i18n and l10n data structure
+const languageStrings = {
+  'de': deData,
+  'en': enData,
+ 
+};
